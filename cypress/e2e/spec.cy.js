ced249378144520d2575828361b5bca91e9eaaec
aliirfan67 <aliirfan672@gmail.com>
2023-06-01 20:45:57 -0500

    test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,7 @@ describe('template spec', () => {
   // }) 
 
   it('passes', () => {
-    cy.visit('https://www.fedex.com/en-us/tracking.html')
+    cy.visit('https://www.fedex.com/en-us/tracking.html');
     // cy.get('body > div.banner > div > h1').should('have.text','Kitchen Sink')
     // cy.get('body > div:nth-child(4) > div > div > ul > li:nth-child(1) > ul > li:nth-child(3) > a').click();
    // cy.get('body > div.banner > div > h1').should('have.text','Sink')
